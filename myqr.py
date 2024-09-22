964532f44288296e7f322666d458774ba4066864
Rehancodecraft <rehan.codecraft@gmail.com>
2024-09-22 12:25:24 +0500

    This is how you can make a qr code in python

---
+++ myqr.py
@@ -0,0 +1,3 @@
+import qrcode
+img = qrcode.make("My name is Rehan shafiq and I am a student of Software Engineering. Here is my my email 'rehan.codecraft@gmail.com' where you can contact me 24/7")
+img.save("myinfo.png","PNG")
